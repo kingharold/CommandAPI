ba15b537a3c304a35afce72d147dcfd488daaa5b
Michael Marotta <subs@michaelmarotta.com>
2021-04-10 12:58:49 -0700

    New Connection settings

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Hosting;
 using CommandAPI.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace CommandAPI
 {
@@ -23,7 +25,15 @@ namespace CommandAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<CommandContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("SqlServerConnection")));
+            var builder = new SqlConnectionStringBuilder();
+
+            builder.ConnectionString = 
+                Configuration.GetConnectionString("SqlServerConnection");
+                builder.UserID = Configuration["UserID"];
+                builder.Password = Configuration["Password"];
+
+
+            services.AddDbContext<CommandContext>(opt => opt.UseSqlServer(builder.ConnectionString));
             services.AddControllers();
 
             //services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
