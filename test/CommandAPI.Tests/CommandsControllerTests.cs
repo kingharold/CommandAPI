31dd905bf36b046bd5c38603290efbf3830b7b41
Michael Marotta <subs@michaelmarotta.com>
2021-09-19 14:24:09 -0700

    revert test result assertation

--- test/CommandAPI.Tests/CommandsControllerTests.cs
+++ test/CommandAPI.Tests/CommandsControllerTests.cs
@@ -279,8 +279,7 @@ namespace CommandAPI.Tests
             var result = controller.DeleteCommand(0);
 
             //Assert
-            //Assert.IsType<NotFoundResult>(result);
-            Assert.IsType<OkResult>(result);
+            Assert.IsType<NotFoundResult>(result);
         }
     }
 }
